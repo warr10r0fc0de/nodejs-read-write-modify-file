914388f5018347e29dc7f21e8fb33a598f329a93
PaulMazur <warriorofcode@gmail.com>
2022-05-13 19:56:46 +0200

    add try..catch to method

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const { readFile, writeFile } = require('fs/promises');
 
 (async () => {
-  const data = await readFile('numbers.txt', 'utf8');
-  const dataArr = data.split('\n');
-  const multipleLastNumberX2 = Number(dataArr[dataArr.length - 1]) * 2;
-
-  await writeFile('numbers.txt', `\n${multipleLastNumberX2}`, { flag: 'a' });
+  try {
+    const data = await readFile('numbers.txt', 'utf8');
+    const dataArr = data.split('\n');
+    const multipleLastNumberX2 = Number(dataArr[dataArr.length - 1]) * 2;
+    await writeFile('numbers.txt', `\n${multipleLastNumberX2}`, { flag: 'a' });
+  } catch (error) {
+    console.log(`${error}`);
+  }
 })();
