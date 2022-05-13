2e4172dd0cfebd9807656b959eb7d1de552068ab
PaulMazur <warriorofcode@gmail.com>
2022-05-13 19:48:27 +0200

    add a method that multiplies the last number by two

---
+++ script.js
@@ -0,0 +1,9 @@
+const { readFile, writeFile } = require('fs/promises');
+
+(async () => {
+  const data = await readFile('numbers.txt', 'utf8');
+  const dataArr = data.split('\n');
+  const multipleLastNumberX2 = Number(dataArr[dataArr.length - 1]) * 2;
+
+  await writeFile('numbers.txt', `\n${multipleLastNumberX2}`, { flag: 'a' });
+})();
